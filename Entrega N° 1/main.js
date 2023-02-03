3c835d4cd422ce993990ffef559a1e8818cf0eb0
Sergio <103077339+SergioJRosales@users.noreply.github.com>
2023-02-02 21:19:39 -0300

    Finalizando

--- Entrega N° 1/main.js
+++ Entrega N° 1/main.js
@@ -13,27 +13,44 @@ let precio4 = 4000;
 
 let nombre = prompt("¡Bienvenido!¿Cuál es tu nombre?");
 
-if (isNaN(nombre)){
-        alert("Hola " + nombre +" ¿Que curso te gustaría realizar con nosotros?" 
+let talleres = ( prompt("Hola " + nombre +" ¿Que curso te gustaría realizar con nosotros?. Digita el número" 
         +"\n 1. " + t1 + " - " + precio1
         +"\n 2. " + t2 + " - " + precio2
         +"\n 3. " + t3 + " - " + precio3
-        +"\n 4. " + t4 + " - " + precio4);
-};
-
-// Elección de talleres
-let talleres = prompt(
-    nombre + ", Ingresá el número del taller que querés realizar"
-   
-)
-// validación del curso elegido
+        +"\n 4. " + t4 + " - " + precio4)
+        );
 if (talleres >= 1 && talleres <= 4);
     if(talleres == 1){
         alert("El taller " + t1 + " tiene un valor de " + precio1);
+        let cantidad = Number(
+            prompt("¿Cuantas membresías de " + t1 + " queres comprar?")
+        );
+        alert("El total a pagar es U$" + precio1 * cantidad);
+    }
+    else if(talleres == 2){
+        alert("El taller " + t2 + " tiene un valor de " + precio2);
+        let cantidad = Number(
+            prompt("¿Cuantas membresías de " + t2 + " queres comprar?")
+        );
+        alert("El total a pagar es U$" + precio2 * cantidad);
+    }
+    else if(talleres == 3){
+        alert("El taller " + t3 + " tiene un valor de " + precio3);
+        let cantidad = Number(
+            prompt("¿Cuantas membresías de " + t3 + " queres comprar?")
+        );
+        alert("El total a pagar es U$" + precio3 * cantidad);
+    }
+    else if(talleres == 4){
+        alert("El taller " + t4 + " tiene un valor de " + precio4);
+        let cantidad = Number(
+            prompt("¿Cuantas membresías de " + t4 + " queres comprar?")
+        );
+        alert("El total a pagar es U$" + precio4 * cantidad);
     }
 else(
     alert("¿Puede ser que te hayas equivocado?")
-)
+);
 
 
 
